e9e8a556a06ac0c4bf475cd60a1e3628492a07e7
zongxinc <zongxinc@bu.edu>
2020-02-22 00:32:47 -0500

    Update test_converter.py

--- test_converter.py
+++ test_converter.py
@@ -1,8 +1,8 @@
 import converter
 
 def test_1():
-	assert converter.Video_compress(['@iGuessPoems','@UnusualPoems', '@PotatoHugots', '@Poem4your_sprog'], 10) == 0
+	assert converter.compressVideo(['@iGuessPoems','@UnusualPoems', '@PotatoHugots', '@Poem4your_sprog'], 10) == 0
 def test_2():
-	assert converter.Video_compress(['@iGuessPoems','@UnusualPoems'], 5) == 0
+	assert converter.compressVideo(['@iGuessPoems','@UnusualPoems'], 5) == 0
 
 
